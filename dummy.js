3def0854894f581a4fc9d3f6e4d553400004335b
YYP <youngyongpark@gmail.com>
2021-04-27 19:16:48 +0900

    Update dependencies (#33)

---
+++ dummy.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+require('axios');
+require('lru-cache');
+require('p-cancelable');
+require('husky');
+require('lint-staged');
+require('react');
+require('react-dom');
+require('react-router-dom');
+require('sass');
+require('query-string');
