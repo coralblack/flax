f0a2f8b491c44eec61fe4ffcf9bcdf54a0d25aab
Hyeyeon Shin <ilxooxli02@naver.com>
2022-02-15 18:09:36 +0900

    Export request module

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ export * from './components/FxButton';
 export * from './components/FxGuard';
 export * from './hooks/useRequest';
 
-export {setDefaultHeaders} from './request';
-export {setBaseUrl} from './request';
-export {setDefaultTransformResponse} from './request';
+export {
+  setDefaultHeaders,
+  setBaseUrl,
+  setDefaultTransformResponse,
+  request,
+} from './request';
