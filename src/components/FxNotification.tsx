8a236a5da80a533bc6756c91c2f3371b5ee4c8a5
Jmoon144 <sjwm98@gmail.com>
2022-04-18 16:43:37 +0900

    err, warn일 경우 notification 고정 추가

--- src/components/FxNotification.tsx
+++ src/components/FxNotification.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import ReactDOM, {render} from 'react-dom';
 import {useNotificationContainer} from './FxNotificationContainer';
+
 import {classNames} from '../utils';
 
 export type FxNotificationType = 'SUCC' | 'WARN' | 'ERROR' | 'INFO';
@@ -31,15 +32,28 @@ export function useNotification(props: UseNotificationProps): UseNotification {
       const wrapper = document.createElement('div');
       wrapper.classList.add('fx-notification-wrapper');
       container.appendChild(wrapper);
+
       render(<FxNotification {...props} />, wrapper);
 
-      setTimeout(() => {
-        wrapper.classList.add('--hide');
-        setTimeout(() => {
-          ReactDOM.unmountComponentAtNode(wrapper);
-          wrapper.remove();
-        }, 450);
-      }, delay || 5000);
+      const closed = document.getElementsByClassName('--closed')[0];
+
+      attrs.type === ('WARN' || 'ERROR')
+        ? closed &&
+          closed.addEventListener('click', () => {
+            wrapper.classList.add('--hide');
+            setTimeout(() => {
+              ReactDOM.unmountComponentAtNode(closed);
+              ReactDOM.unmountComponentAtNode(wrapper);
+              wrapper.remove();
+            }, 500);
+          })
+        : setTimeout(() => {
+            wrapper.classList.add('--hide');
+            setTimeout(() => {
+              ReactDOM.unmountComponentAtNode(wrapper);
+              wrapper.remove();
+            }, 450);
+          }, delay || 3000);
     },
   };
 }
@@ -65,34 +79,40 @@ function Type(type: FxNotificationType) {
         </svg>
       )}
       {type === 'WARN' && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-          />
-        </svg>
+        <>
+          <span className="--closed">x</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+        </>
       )}
       {type === 'ERROR' && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
+        <>
+          <span className="--closed">x</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </>
       )}
     </div>
   );
